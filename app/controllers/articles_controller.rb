a2a086314c11a286f8039bc128a43cbef320a8a6
Martin <martin@kreditz.com>
2023-02-05 11:47:48 +0100

    update view users (edit, view)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -18,7 +18,7 @@ class ArticlesController < ApplicationController
 
 
   def create
-    @article = Article.new(article_params)
+    @article = A rticle.new(article_params)
     @article.user = User.first
     if @article.save
       flash[:notice] = "Article was created successfully."
