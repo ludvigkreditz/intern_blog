13015155a6732cc0f70617fa32dc6e7b2ec1e828
Martin <martin@kreditz.com>
2023-02-07 12:31:02 +0100

    Add admin user access

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_02_05_110443) do
+ActiveRecord::Schema[7.0].define(version: 2023_02_07_101939) do
   create_table "articles", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_02_05_110443) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "password_digest"
+    t.boolean "admin", default: false
   end
 
 end
